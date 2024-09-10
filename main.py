85f0b7ccced6dbfb45af455dd1d5a4392e73ac67
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-09-10 23:30:26 +0330

    add Target class

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-import math
-import random
-import time
 import pygame
 
 
 pygame.init()
 
-width, higth = 600, 400 
+width, higth = 800, 600 
 window = pygame.display.set_mode(size=(width, higth)) # مشخص کردن اندازه صفحه ی بازی
 pygame.display.set_caption("Aim Trainer") # عنوان بازی بر روی صفحه ی بازی
 
 
+class Target:
+    max_size = 30 # حداکثر تعداد پیکسل که هر هدف بهش میرسه
+    growth_rate = 0.2 # سرعت رشد هر هدف در صفحه ی بازی
+    color = "red" # رنگ اهداف در صفحه ی بازی
+
+    def __init__(self, x, y): # موقعیت مکانی و اندازه اولیه هر هدفی که ساخته می شود در اینجا تعیین می شود
+        self.x = x
+        self.y = y
+        self.size = 0
+        self.grow = True
+        
+    def update(self):
+        if self.size + self.growth_rate >= self.max_size: # اندازه اهداف به 30 رسید دیگر بزرگتر نمی شوند
+            self.grow = False
+
+        if self.grow: 
+            self.size += self.growth_rate #  اندازه اهداف تا به 30 برسند دائما بزرگتر می شوند
+        else:
+            self.size -= self.growth_rate # اندازه اهداف وقتی به 30 برسند دائما کوچک تر می شوند         
+
+
+
 def main():
     """_summary_
-    create a infinity loop for showing the window.
+    create a infinity loop for showing the window
     """
     run = True
     while run:
